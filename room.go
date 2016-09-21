5a88d3c1dcc37f3bf84892faf0c431506658878d
青柳 恭義 <takayoshi_aoyagi@waku-2.com>
2016-09-21 19:00:21 +0900

    1.2.3

--- room.go
+++ room.go
@@ -2,4 +2,33 @@ package main
 
 type room struct {
 	forward chan []byte
+	join chan *client
+	leave chan *client
+	clients map[*client]bool
 }
+
+func (r * room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(coient.send)
+		case msg := <-r.forward:
+			// Broadcast
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send message
+				default:
+					// send failure
+					delete(r.clients
+						close(client.send)
+					}
+				}
+			}
+		}
+	}
+}
+
